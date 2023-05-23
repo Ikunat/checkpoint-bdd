83dd080f54339a53d8114738dc1d2d784a2b2ece
Ikunat <francois.deleau@pm.me>
2023-05-23 16:35:29 +0200

    🔥 step 2 🔥

--- queries/1_insert_data.sql
+++ queries/1_insert_data.sql
@@ -2,8 +2,12 @@
 # N'oublie pas, les requêtes SQL doivent se terminer avec un ";"
 # Pense à céer au moins une entreprise nommée "WCS", à créer des offre d'emploi à "Paris" dont une qui s'intitule "Dev"
 
+# ENTREPRISES
+
 INSERT INTO entreprise (name, description)
 VALUES ('WCS', 'Wild Code School');
+
+# JOBS
 
 INSERT INTO job (title, description, city)
 VALUES ('Dev','Debug this app you piece of ****', 'Paris');
@@ -13,5 +17,18 @@ VALUES ('Cleaner','Clean this floor you piece of ****', 'Paris');
 
 INSERT INTO job (title, description, city)
 VALUES ('Politician','Read this paper you piece of ****', 'Paris');
+
+# CANDIDATES
+
+INSERT INTO candidate (password, firstname, lastname, email, description, jobID)
+VALUES ('password1', 'Obi-Wan', 'Kenobi', 'john@doe1.fr', 'That\s me B**ch what else would you need to know', 1);
+
+INSERT INTO candidate (password, firstname, lastname, email, description, jobID)
+VALUES ('password2', 'Anakin', 'Skywalker', 'john@doe2.fr', 'That\s me B**ch what else would you need to know', 1);
+
+INSERT INTO candidate (password, firstname, lastname, email, description, jobID)
+VALUES ('password3', 'Qui-Gon', 'Jinn', 'john@doe3.fr', 'That\s me B**ch what else would you need to know', 1);
+
+
 
 SELECT * FROM job;
--- queries/2_all_candidates_for_job.sql
+++ queries/2_all_candidates_for_job.sql
@@ -1,2 +1,5 @@
 # Écris une requêtes SQL qui retourne tous les candidats ayant répondu à l'offre
 # intitulée "Dev"
+
+SELECT * FROM candidate
+WHERE jobID = 1;
